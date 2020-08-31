85b225322783c4d2906dac3a929d2bf0f23a1267
Kirill Ponur <ponur0kirill@gmail.com>
2020-08-31 22:27:15 +0300

    add swell

--- cudakernel.py
+++ cudakernel.py
@@ -4,10 +4,10 @@ import numpy
 
 from pycuda.compiler import SourceModule
 mod = SourceModule("""
-__global__ void multiply_them(float *dest, float *a, float *b)
+__global__ void multiply_them(int dest, float *a, float *b)
 {
   const int i = threadIdx.x;
-  dest[i] = a[i] * b[i];
+  dest = sizeof(a)/sizeof(*a);
 
 }
 """)
@@ -23,3 +23,98 @@ multiply_them(
         block=(400,1,1), grid=(1,1))
 
 print(dest-a*b)
+
+
+# from numba import  cuda
+# import numpy as np
+# import math
+
+# TPB=16
+
+
+# @cuda.jit
+# def kernel_cwm(ans, x, y, k, phi, A, F, psi):
+    # i = cuda.grid(1)
+
+    # if i >= x.shape[0]:
+        # return
+
+    # for n in range(k.size):
+        # for m in range(phi.size):
+                # kr = k[n]*(x[i]*math.cos(phi[m]) + y[i]*math.sin(phi[m]))
+                # Af = A[n] * F[n][m]
+                # Cos =  math.cos(kr + psi[n][m]) * Af
+                # Sin =  math.sin(kr + psi[n][m]) * Af
+
+                # kx = k[n] * math.cos(phi[m])
+                # ky = k[n] * math.sin(phi[m])
+
+
+                # # Высоты (z)
+                # ans[0,i] +=  Cos
+                # # Наклоны X (dz/dx)
+                # ans[1,i] +=  -Sin * kx
+                # # Наклоны Y (dz/dy)
+                # ans[2,i] +=  -Sin * ky
+
+                # # CWM
+                # x[i] += -Sin * math.cos(phi[m])
+                # y[i] += -Sin * math.sin(phi[m])
+                # ans[1,i] *= 1 - Cos * math.cos(phi[m]) * kx
+                # ans[2,i] *= 1 - Cos * math.sin(phi[m]) * ky
+
+
+
+# @cuda.jit
+# def kernel_default(ans, x, y, k, phi, A, F, psi):
+    # i = cuda.grid(1)
+
+    # if i >= x.shape[0]:
+        # return
+
+    # for n in range(k.size):
+        # for m in range(phi.size):
+                # kr = k[n]*(x[i]*math.cos(phi[m]) + y[i]*math.sin(phi[m]))
+                # Af = A[n] * F[n][m]
+                # Cos =  math.cos(kr + psi[n][m]) * Af
+                # Sin =  math.sin(kr + psi[n][m]) * Af
+
+                # kx = k[n] * math.cos(phi[m])
+                # ky = k[n] * math.sin(phi[m])
+
+
+                # # Высоты (z)
+                # ans[0,i] +=  Cos
+                # # Наклоны X (dz/dx)
+                # ans[1,i] +=  -Sin * kx
+                # # Наклоны Y (dz/dy)
+                # ans[2,i] +=  -Sin * ky
+
+# @cuda.jit
+# def kernel_polar(ans, x, y, k, phi, A, F, psi):
+    # i = cuda.grid(1)
+
+    # if i >= x.size:
+        # return
+
+    # for n in range(k.size):
+        # for m in range(phi.size):
+                # kr = k[n]*(x[i]*math.cos(phi[m]) + y[i]*math.sin(phi[m]))
+                # Af = A[n] * F[n][m]
+                # Cos =  math.cos(kr + psi[n][m]) * Af
+                # Sin = - math.sin(kr + psi[n][m]) * Af
+
+                # Cosx = k[n] * math.cos(phi[m])
+                # Siny = k[n] * math.sin(phi[m])
+
+
+                # ans[0,i] +=  Cos
+                # ans[1,i] +=  Cos * Cosx
+                # ans[2,i] +=  Sin * Siny
+
+                # # CWM
+                # ans[1,i] *= 1 - Cos * Cosx
+                # ans[2,i] *= 1 - Sin * Siny
+
+                # x[i] += Sin * math.cos(phi[m])
+                # y[i] += Sin * math.sin(phi[m])
