7ae82b97e0758f3c930afbd0d0d40c21d26eda30
Kirill Ponur <ponur0kirill@gmail.com>
2020-09-03 17:10:51 +0300

    firth check

--- generate_impulses.py
+++ generate_impulses.py
@@ -24,83 +24,83 @@ surface = Surface(const)
 host_constants = surface.export()
 print(host_constants[-1].shape)
 print(host_constants[-1])
-# stream = cuda.stream()
-# k, phi, A, F, psi = (cuda.to_device(host_constants[i], stream = stream) for i in range(len(host_constants)))
+stream = cuda.stream()
+k, phi, A, F, psi = (cuda.to_device(host_constants[i], stream = stream) for i in range(len(host_constants)))
 
 
-# Hs = 4 * np.sqrt(surface.sigma_sqr)
-# xmax = 4 * np.sqrt(Hs * z0)
-# print(xmax)
+Hs = 4 * np.sqrt(surface.sigma_sqr)
+xmax = 4 * np.sqrt(Hs * z0)
+print(xmax)
 
-# x0 = np.linspace(-xmax, xmax, grid_size)
-# y0 = np.linspace(-xmax, xmax, grid_size)
-# x0, y0 = np.meshgrid(x0,y0)
-# x0 = x0.flatten()
-# y0 = y0.flatten()
-
-
-# # threadsperblock = TPB 
-# blockspergrid = math.ceil(x0.size / threadsperblock)
-# # kernels = [kernel_default, kernel_cwm]
-# labels = ["default", "cwm"] 
-# fig, ax = plt.subplots()
+x0 = np.linspace(-xmax, xmax, grid_size)
+y0 = np.linspace(-xmax, xmax, grid_size)
+x0, y0 = np.meshgrid(x0,y0)
+x0 = x0.flatten()
+y0 = y0.flatten()
 
 
-# data_p = pd.Series(dtype='object')
-# data_s = {}
-# data_m = pd.Series(dtype='object')
-
-# mn = []
-# disp = []
-
-# T0 = (z0 - 5)/c
-
-# for j, kernel in enumerate(kernels):
+threadsperblock = TPB 
+blockspergrid = math.ceil(x0.size / threadsperblock)
+kernels = [kernel_default, kernel_cwm]
+labels = ["default", "cwm"] 
+fig, ax = plt.subplots()
 
 
-#     surf = np.zeros((2, 3, x0.size))
-#     stream = cuda.stream()
-#     kernel[blockspergrid, threadsperblock, stream](surf, x0, y0, k, phi, A, F, psi)
+data_p = pd.Series(dtype='object')
+data_s = {}
+data_m = pd.Series(dtype='object')
+
+mn = []
+disp = []
+
+T0 = (z0 - 5)/c
+
+for j, kernel in enumerate(kernels):
 
 
-#     T = np.linspace(T0-Hs*1.2/c, np.sqrt(z0**2+xmax**2)/c, 104)
-#     P = np.zeros(T.size)
-
-#     pulse = Pulse(surf, x0, y0, const)
-
-#     index = pulse.mirror_sort()
-#     for i in range(T.size):
-#         P[i] = pulse.power1(T[i])
+    surf = np.zeros((2, 3, x0.size))
+    stream = cuda.stream()
+    kernel[blockspergrid, threadsperblock, stream](surf, x0, y0, k, phi, A, F, psi)
 
 
-#     dT = pd.Series(T - T0, name = 't_%s' % (labels[j]))
-#     dP = pd.Series(P/P.max(), name = 'P_%s' % (labels[j]))
-#     data_p = pd.concat([data_p, dT, dP], axis=1)
+    T = np.linspace(T0-Hs*1.2/c, np.sqrt(z0**2+xmax**2)/c, 104)
+    P = np.zeros(T.size)
 
-#     ax.plot(T-T0, P/P.max(), label = labels[j])
+    pulse = Pulse(surf, x0, y0, const)
+
+    index = pulse.mirror_sort()
+    for i in range(T.size):
+        P[i] = pulse.power1(T[i])
 
 
+    dT = pd.Series(T - T0, name = 't_%s' % (labels[j]))
+    dP = pd.Series(P/P.max(), name = 'P_%s' % (labels[j]))
+    data_p = pd.concat([data_p, dT, dP], axis=1)
 
-
-
-# now = datetime.datetime.now().strftime("%m%d_%H%M")
-# os.mkdir(str(now))
-# data_p = pd.DataFrame(data_p)
+    ax.plot(T-T0, P/P.max(), label = labels[j])
 
 
 
 
-# ax.set_xlabel('t')
-# ax.set_ylabel('P')
-# plt.legend()
-# plt.savefig('%s/impulse.png' % (now))
 
-# data_p.to_csv('%s/impulse%d_%s.csv' % (now, const["wind.speed"][0], now), index = False, sep=';')
+now = datetime.datetime.now().strftime("%m%d_%H%M")
+os.mkdir(str(now))
+data_p = pd.DataFrame(data_p)
 
 
 
-# with open('%s/rc.json' % (now), 'w', encoding="utf-8") as f:
-#     dump(const, f, indent=4)
+
+ax.set_xlabel('t')
+ax.set_ylabel('P')
+plt.legend()
+plt.savefig('%s/impulse.png' % (now))
+
+data_p.to_csv('%s/impulse%d_%s.csv' % (now, const["wind.speed"][0], now), index = False, sep=';')
 
 
-# plt.show()
+
+with open('%s/rc.json' % (now), 'w', encoding="utf-8") as f:
+    dump(const, f, indent=4)
+
+
+plt.show()
